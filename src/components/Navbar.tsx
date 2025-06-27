ffe779cab9cd4627873747437388d30e1599436e
adammulder <119084362+adammulder@users.noreply.github.com>
2025-06-27 16:25:14 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,8 +30,7 @@ const Navbar = () => {
     <>
     
     { isHome ? (
-    <div className="bg-[url('https://images.unsplash.com/photo-1549125764-91425ca48850?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
-        h-auto">
+    <div className="bg-[url('./assets/images/mountain-6915304_1280.jpg')]">
         <div className='flex place-content-center place-items-center text-slate-100 text-xl h-[94vh] sm:h-[91vh] '>
             <h1 className='font-link text-6xl sm:text-8xl'>PictoShaman</h1>
         </div>
@@ -43,7 +42,7 @@ const Navbar = () => {
         </div>
     </div>
     ) : (
-    <div className="bg-[url('https://images.unsplash.com/photo-1549125764-91425ca48850?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover">
+    <div className="bg-[url('./assets/images/mountain-6915304_1280.jpg')] bg-cover">
     <div className='flex place-content-center place-items-center text-white sm:text-slate-500 text-xl h-40 justify-between'>
     <h1 className='font-link text-4xl p-5 sm:text-8xl'>PictoShaman</h1>
     <button onClick={() => setIsopen((isOpen) => !isOpen)} className='flex justify-end justify-items-end p-5 text-3xl hover:text-green-500'><FaBars></FaBars></button>
